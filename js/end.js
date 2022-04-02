9ec5afe6090d7e67571dce72a72413495668f67e
Angell022 <jass461489@gmail.com>
2022-04-01 20:49:55 -0600

    score2

--- js/end.js
+++ js/end.js
@@ -27,5 +27,5 @@ saveHighScore = e => {
   highScores.splice(4)
 
   localStorage.setItem('highScores', JSON.stringify(highScores))
-  window.location.assign('/')
+  window.location.assign('../index.html')
 }
